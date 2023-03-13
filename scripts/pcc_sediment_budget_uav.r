ced4f3938ebad4dbeb7e6f54f826241984a38150
gubely <yves.gubelmannrgmx.ch>
2023-03-13 17:10:58 +0100

    Added paths. Duplicated code.

--- scripts/pcc_sediment_budget_uav.r
+++ scripts/pcc_sediment_budget_uav.r
@@ -90,6 +90,12 @@ t0_output_id <- as.character(paste(timestamp, t0_config_id, sep = "-"))
 t0_output_path <- file.path(dir_data, dir_persp, t0_year, settype, "output", t0_output_id, fsep="/")
 t0_config_json_name <- as.character(paste(t0_config_id, ".json", sep = ""))
 t0_config_json_path <- file.path(t0_dir_config, t0_config_json_name, fsep="/")
+t0_data_path <- file.path(dir_data, dir_persp, t0_year, settype, t0_dataset)
+# load dataset specific parameter set
+t0_cfg <- fromJSON(file = t0_config_json_path)
+t0_mcdut_shp_name <- as.character(paste(t0_cfg$mapcurve_dut_shp, sep = ""))
+t0_mcdut_path <- file.path(dir_repo, t0_mcdut_shp_name, fsep="/")
+
 
 # Settings t1
 t1_dataset_id <- "1"
@@ -102,65 +108,60 @@ t1_output_id <- as.character(paste(timestamp, t1_config_id, sep = "-"))
 t1_output_path <- file.path(dir_data, dir_persp, t1_year, settype, "output", t1_output_id, fsep="/")
 t1_config_json_name <- as.character(paste(t1_config_id, ".json", sep = ""))
 t1_config_json_path <- file.path(t1_dir_config, t1_config_json_name, fsep="/")
+t1_data_path <- file.path(dir_data, dir_persp, t1_year, settype, t1_dataset)
+# load dataset specific parameter set
+t1_cfg <- fromJSON(file = t1_config_json_path)
+t1_mcdut_shp_name <- as.character(paste(t1_cfg$mapcurve_dut_shp, sep = ""))
+t1_mcdut_path <- file.path(dir_repo, t1_mcdut_shp_name, fsep="/")
+
 
 aoi_path <- file.path(dir_repo, "data/area_of_interest_final", "AOI_final.shp", fsep="/")
 aoi_dir <- file.path(dir_repo, "data/area_of_interest_final", fsep="/")
 bud_output_path <- file.path(dir_data, dir_persp, "budget", timestamp, fsep="/")
-
-
-# load dataset specific parameter set
-t0_cfg <- fromJSON(file = t0_config_json_path)
-t1_cfg <- fromJSON(file = t1_config_json_path)
 
 # Create run specific output folder
 dir.create(bud_output_path)
 dir.create(t0_output_path)
 dir.create(t1_output_path)
 
-mctar_shp_name <- as.character(paste(cfg$mapcurve_target_shp, sep = ""))
-mctar_path <- file.path(dir_repo, mctar_shp_name, fsep="/")
-
-mcdut_shp_name <- as.character(paste(cfg$mapcurve_dut_shp, sep = ""))
-mcdut_path <- file.path(dir_repo, mcdut_shp_name, fsep="/")
-
-output_json_name <- as.character(paste(output_id, ".json", sep = ""))
-output_json_path <- file.path(output_path, output_json_name, fsep="/")
-
-output_target_name <- as.character(paste(output_id, ".png", sep = ""))
-output_target_path <- file.path(output_path, output_target_name, fsep="/")
-
-output_csv_name <- as.character(paste(output_id, ".csv", sep = ""))
-output_csv_path <- file.path(output_path, output_csv_name, fsep="/")
-
-output_las_inp_name <- as.character(paste(output_id, "-inp.txt", sep = ""))
-output_las_inp_path <- file.path(output_path, output_las_inp_name, fsep="/")
-
-output_las_aoi_name <- as.character(paste(output_id, "-aoi.txt", sep = ""))
-output_las_aoi_path <- file.path(output_path, output_las_aoi_name, fsep="/")
-
-output_las_class_name <- as.character(paste(output_id, "-classified.txt", sep = ""))
-output_las_class_path <- file.path(output_path, output_las_class_name, fsep="/")
-
-output_asc_name <- as.character(paste(output_id, ".asc", sep = ""))
-output_asc_path <- file.path(output_path, output_asc_name, fsep="/")
-
-output_ncdf_name <- as.character(paste(output_id, ".nc", sep = ""))
-output_ncdf_path <- file.path(output_path, output_ncdf_name, fsep="/")
-
-output_las_sed_name <- as.character(paste(output_id, "-sed.las", sep = ""))
-output_las_sed_path <- file.path(output_path, output_las_sed_name, fsep="/")
-
-output_las_gnd_name <- as.character(paste(output_id, "-gnd.las", sep = ""))
-output_las_gnd_path <- file.path(output_path, output_las_gnd_name, fsep="/")
-
-output_las_all_name <- as.character(paste(output_id, "-all.las", sep = ""))
-output_las_all_path <- file.path(output_path, output_las_all_name, fsep="/")
-
-output_target_rast_name <- as.character(paste(output_id, "-target.png", sep = ""))
-output_target_rast_path <- file.path(output_path, output_target_rast_name, fsep="/")
-
-t0_data_path <- file.path(dir_data, dir_persp, t0_year, settype, t0_dataset)
-t1_data_path <- file.path(dir_data, dir_persp, t1_year, settype, t1_dataset)
+# mctar_shp_name <- as.character(paste(cfg$mapcurve_target_shp, sep = ""))
+# mctar_path <- file.path(dir_repo, mctar_shp_name, fsep="/")
+# 
+# output_json_name <- as.character(paste(output_id, ".json", sep = ""))
+# output_json_path <- file.path(output_path, output_json_name, fsep="/")
+# 
+# output_target_name <- as.character(paste(output_id, ".png", sep = ""))
+# output_target_path <- file.path(output_path, output_target_name, fsep="/")
+# 
+# output_csv_name <- as.character(paste(output_id, ".csv", sep = ""))
+# output_csv_path <- file.path(output_path, output_csv_name, fsep="/")
+# 
+# output_las_inp_name <- as.character(paste(output_id, "-inp.txt", sep = ""))
+# output_las_inp_path <- file.path(output_path, output_las_inp_name, fsep="/")
+# 
+# output_las_aoi_name <- as.character(paste(output_id, "-aoi.txt", sep = ""))
+# output_las_aoi_path <- file.path(output_path, output_las_aoi_name, fsep="/")
+# 
+# output_las_class_name <- as.character(paste(output_id, "-classified.txt", sep = ""))
+# output_las_class_path <- file.path(output_path, output_las_class_name, fsep="/")
+# 
+# output_asc_name <- as.character(paste(output_id, ".asc", sep = ""))
+# output_asc_path <- file.path(output_path, output_asc_name, fsep="/")
+# 
+# output_ncdf_name <- as.character(paste(output_id, ".nc", sep = ""))
+# output_ncdf_path <- file.path(output_path, output_ncdf_name, fsep="/")
+# 
+# output_las_sed_name <- as.character(paste(output_id, "-sed.las", sep = ""))
+# output_las_sed_path <- file.path(output_path, output_las_sed_name, fsep="/")
+# 
+# output_las_gnd_name <- as.character(paste(output_id, "-gnd.las", sep = ""))
+# output_las_gnd_path <- file.path(output_path, output_las_gnd_name, fsep="/")
+# 
+# output_las_all_name <- as.character(paste(output_id, "-all.las", sep = ""))
+# output_las_all_path <- file.path(output_path, output_las_all_name, fsep="/")
+# 
+# output_target_rast_name <- as.character(paste(output_id, "-target.png", sep = ""))
+# output_target_rast_path <- file.path(output_path, output_target_rast_name, fsep="/")
 
 # Read files--------------------------------------------------------------------
 
@@ -185,20 +186,21 @@ bounding_box <- gen_xy %>%
   st_cast("POLYGON")
 
 # Read Shapefiles
-mctar_all <- read_sf(dsn = mctar_path) 
-mcdut <- read_sf(dsn = mcdut_path) 
+# mctar_all <- read_sf(dsn = mctar_path) 
+t0_mcdut <- read_sf(dsn = t0_mcdut_path)
+t1_mcdut <- read_sf(dsn = t1_mcdut_path)
 aoi_shp <- read_sf(dsn = aoi_path)
 
 # Intersect target with area of interest
-mctar_bb <- st_intersection(mctar_all, bounding_box)
+# mctar_bb <- st_intersection(mctar_all, bounding_box)
 
 # Separate targets filtered by class
-mctar_water <- mctar_bb %>%  filter(Id == 1)
-mctar_sed <- mctar_bb %>%  filter(Id == 2)
-mctar_veg <- mctar_bb %>%  filter(Id == 3)
-mctar_rock <- mctar_bb %>%  filter(Id == 5)
+# mctar_water <- mctar_bb %>%  filter(Id == 1)
+# mctar_sed <- mctar_bb %>%  filter(Id == 2)
+# mctar_veg <- mctar_bb %>%  filter(Id == 3)
+# mctar_rock <- mctar_bb %>%  filter(Id == 5)
 
-targeted_class <- mctar_sed
+# targeted_class <- mctar_sed
 
 # Generate target beforehand with raster----------------------------------------
 # Calculate number of rows and columns of target, based on extent
@@ -212,7 +214,6 @@ targeted_class <- mctar_sed
 # Information about rasterized target
 # target$layer
 # st_crs(target)
-
 
 # Plot features on one plot
 # ggplot() + 
@@ -229,7 +230,8 @@ targeted_class <- mctar_sed
 # Read LAS
 # Intensity (i), color information (RGB), number of Returns (r), classification (c)
 # of the first point is loaded only to reduce computational time.
-las <- readLAS(data_path, select = "xyzRGBc", filter = cfg$las_filter)
+t0_las <- readLAS(data_path, select = "xyzRGBc", filter = t0_cfg$las_filter)
+t1_las <- readLAS(data_path, select = "xyzRGBc", filter = t1_cfg$las_filter)
 
 # Filter points which are not within area of interest---------------------------
 # las <- classify_poi(las, class = LASNOISE, roi = aoi_shp, inverse_roi = T)
@@ -237,48 +239,61 @@ las <- readLAS(data_path, select = "xyzRGBc", filter = cfg$las_filter)
 # plot(las, size = 1, color = "RGB", bg = "white", axis = F)
 
 # Reset class LASNOISE for further procedure
-las$Classification <- LASNONCLASSIFIED
+t0_las$Classification <- LASNONCLASSIFIED
+t1_las$Classification <- LASNONCLASSIFIED
 
 # Create copy of read LAS to omit loading procedure.
 # las_origin <- las
 # las <- las_origin
 
-# Check LAS whether it complies with the required
-if(has.lasClassification(las)){
-  print("LAS file is already classified. Are you sure to continue?")}
-# if (length(warnings())>=1) {stop("The read LAS file throws warnings, script stops.")}
-
-# Data exploration--------------------------------------------------------------
-data_path
-
-# Segment Water with Cloth Simulation Filter-----------------------------------
+# Segment Water with Cloth Simulation Filter------------------------------------
+# Global filter settings for both years. Can be changed (implementation in json)
 rigid_n_water <- 1
 class_thres_water <- 0.2
 cloth_res_water <- 3.9
-status_water <- as.character(paste("00_water", "_rigid", rigid_n_water,
+t0_status_water <- as.character(paste("00_water", t0_year, "_rigid", rigid_n_water,
                                    "_clthres", class_thres_water,
                                    "clothres", cloth_res_water, sep = ""))
-las_water <- classify.gnd(las, class_thres_water, cloth_res_water, rigid_n_water)
-las_water <- add_attribute(las_water, FALSE, "water")
-las_water$water <- if_else(las_water$Classification == LASGROUND, T, F)
+t1_status_water <- as.character(paste("00_water", t1_year, "_rigid", rigid_n_water,
+                                      "_clthres", class_thres_water,
+                                      "clothres", cloth_res_water, sep = ""))
 
-las_water <- filter_poi(las_water, Classification == LASGROUND)
+t0_las_water <- classify.gnd(t0_las, class_thres_water, cloth_res_water, rigid_n_water)
+t1_las_water <- classify.gnd(t1_las, class_thres_water, cloth_res_water, rigid_n_water)
+
+t0_las_water <- add_attribute(t0_las_water, FALSE, "water")
+t1_las_water <- add_attribute(t1_las_water, FALSE, "water")
+t0_las_water$water <- if_else(t0_las_water$Classification == LASGROUND, T, F)
+t1_las_water$water <- if_else(t1_las_water$Classification == LASGROUND, T, F)
+
+t0_las_water <- filter_poi(t0_las_water, Classification == LASGROUND)
+t1_las_water <- filter_poi(t1_las_water, Classification == LASGROUND)
 # plot(las_water, size = 1, color = "RGB", bg = "white", axis = F)
 
 # Filter points which are not within area of interest
-las_water <- classify_poi(las_water, class = LASNOISE, roi = mcdut, inverse_roi = T)
-las_water <- filter_poi(las_water, Classification != LASNOISE)
-plot(las_water, size = 1, color = "RGB", bg = "black", axis = F)
+t0_las_water <- classify_poi(t0_las_water, class = LASNOISE, roi = t0_mcdut, inverse_roi = T)
+t0_las_water <- filter_poi(t0_las_water, Classification != LASNOISE)
+plot(t0_las_water, size = 1, color = "RGB", bg = "black", axis = F)
 set.RGLtopview()
-output_water_png_name <- as.character(paste(status_water, ".png", sep = ""))
-output_water_png_path <- file.path(output_path, output_water_png_name, fsep="/")
+t0_output_water_png_name <- as.character(paste(t0_status_water, ".png", sep = ""))
+t0_output_water_png_path <- file.path(bud_output_path, t0_output_water_png_name, fsep="/")
+rgl.snapshot(t0_output_water_png_path)
+rgl.close()
 
-rgl.snapshot(output_water_png_path)
+t1_las_water <- classify_poi(t1_las_water, class = LASNOISE, roi = t1_mcdut, inverse_roi = T)
+t1_las_water <- filter_poi(t1_las_water, Classification != LASNOISE)
+plot(t1_las_water, size = 1, color = "RGB", bg = "black", axis = F)
+set.RGLtopview()
+t1_output_water_png_name <- as.character(paste(t1_status_water, ".png", sep = ""))
+t1_output_water_png_path <- file.path(bud_output_path, t1_output_water_png_name, fsep="/")
+rgl.snapshot(t1_output_water_png_path)
 rgl.close()
 
 # Rasterize water point cloud    
-DEM_water <- rasterize_canopy(las_water, res = raster_res, p2r(), pkg = "raster")
+t0_DEM_water <- rasterize_canopy(t0_las_water, res = raster_res, p2r(), pkg = "raster")
+t1_DEM_water <- rasterize_canopy(t1_las_water, res = raster_res, p2r(), pkg = "raster")
 
+# tbd
 raster_ext <- extent(xmin(DEM_water), xmax(DEM_water), ymin(DEM_water), ymax(DEM_water))
 raster_water <- raster(nrows=nrow(DEM_water), ncols=ncols(DEM_water), crs=2056,
                   ext=raster_ext, resolution=raster_res, vals=NULL)
